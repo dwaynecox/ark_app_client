d1cf4817bee7fdf233a5fa6ec992edaa8df61747
dwayne <dwayneedwardcox@gmail.com>
2018-07-11 22:22:55 -0500

    debut auth working

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # get '/' => 'client/products#index'
+  namespace :client do
+    # get '/products' => 'products#index'
+    # get '/products/new' => 'products#new'
+    # post '/products' => 'products#create'
+    # get '/products/:id' => 'products#show'
+    # get '/products/:id/edit' => 'products#edit'
+    # patch '/products/:id' => 'products#update'
+    # delete '/products/:id' => 'products#destroy'
+  end
+  # get "/signup" => "users#new"
+  # get "/users" => "users#new"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+end
